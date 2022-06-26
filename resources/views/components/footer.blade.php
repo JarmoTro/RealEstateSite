9ddf994202ac13bb28c813d2a1e6c1e838cdcde9
JarmoTro <82982361+JarmoTro@users.noreply.github.com>
2022-06-26 15:00:33 +0300

    add header and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,18 @@
+
+<hr class="my-6 sm:mx-auto dark:border-gray-700 lg:my-8" />
+
+<footer>
+    <span class="block text-gray-500 text-center">
+
+        <p class="inline mr-2"> Jarmo T. &#169; 2022 </p>
+        
+        <a class="mr-2" href="https://github.com/JarmoTro"><i class="hover:text-gray-600 fa fa-brands fa-github-square fa-lg"></i></a>
+
+        <a class="mr-2" href="https://www.linkedin.com/in/jarmo-troska-71117b241/"><i class="hover:text-gray-600 fa fa-brands fa-linkedin fa-lg"></i></a>
+
+    </span>
+</footer>
+
+<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+</body>
+</html>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Real Estate</title>
+    <link rel="stylesheet" href="https://unpkg.com/flowbite@1.4.7/dist/flowbite.min.css" />
+    <script src="https://kit.fontawesome.com/51dc6e0b3e.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="/" class="flex items-center">
+              <i class="fa fa-solid fa-house fa-2xl mr-2"></i>
+              <span class="self-center text-4xl font-semibold whitespace-nowrap dark:text-white"><h4 class="text-3xl">Real Estate</h4></span>
+          </a>
+          <div class="flex md:order-2">
+            <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
+              <span class="sr-only">Open main menu</span>
+              <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            </button>
+          </div>
+          <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a href="#" class="block text-2xl py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" class="block text-2xl py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Apartments</a>
+              </li>
+              <li>
+                <a href="#" class="block text-2xl py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Houses</a>
+              </li>
+              <li>
+                <a href="#" class="block text-2xl py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Land</a>
+              </li>
+              <li>
+                <a href="#" class="block text-2xl py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Other</a>
+              </li>
+              <li>
+                <button type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+              </li>
+              <li>
+                <button type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-2 md:mt-0 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add Listing</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      
+
+    <header class="max-w-xl mx-auto mt-4 text-center">
+        <h1 class="text-4xl">
+            The Hottest <span class="text-blue-500">Real Estate</span> In Your Area
+        </h1>
+
+    </header>
